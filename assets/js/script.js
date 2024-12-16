41aa4d85b2266e1ae39416bb6ca8ce3cbe088eac
RANAIVOSON Nantenaina Claudio <ranaivosonclaudio@gmail.com>
2024-12-16 10:20:05 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,45 @@ navbarToggler.addEventListener('click', () => {
     navbarCollapse.classList.toggle('collapse');
 });
 
+const texts = [
+    "Moi, c'est RANAIVOSON Nantenaina Claudio", // #2c3e50
+    "Développeur Web", // #3498db
+    "Bienvenue dans mon univers créatif", // #e74c3c
+    "Imaginez et construisons ensemble" // #2ecc71
+];
+const typingSpeed = 100;
+const delayBetweenTexts = 2000;
+const textDisplay = document.getElementById("text-display");
+let currentTextIndex = 0;
+let currentCharIndex = 0;
+const textColors = [
+    "#2c3e50", 
+    "#3498db", 
+    "#e74c3c", 
+    "#2ecc71"
+];
+function typeText() {
+    if (currentCharIndex < texts[currentTextIndex].length) {
+        textDisplay.textContent += texts[currentTextIndex][currentCharIndex];
+        textDisplay.style.color = textColors[currentTextIndex];
+        currentCharIndex++;
+        setTimeout(typeText, typingSpeed);
+    } else {
+        setTimeout(deleteText, delayBetweenTexts);
+    }
+}
+function deleteText() {
+    if (currentCharIndex > 0) {
+        textDisplay.textContent = texts[currentTextIndex].substring(0, currentCharIndex - 1);
+        currentCharIndex--;
+        setTimeout(deleteText, typingSpeed);
+    } else {
+        currentTextIndex = (currentTextIndex + 1) % texts.length;
+        setTimeout(typeText, typingSpeed);
+    }
+}
+typeText();
+  
 document.addEventListener("DOMContentLoaded", function () {
     const images = document.querySelectorAll(".project-img");
     images.forEach(image => {
